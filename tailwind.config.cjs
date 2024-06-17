a48b8dc5b5b7094f7e2e3e11fef56806ff7764df
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-06-17 14:27:20 +0900

    chore: 폰트 설정

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { meetme: 'Ownglyph_meetme-Rg' },
+    },
   },
   plugins: [],
 };
