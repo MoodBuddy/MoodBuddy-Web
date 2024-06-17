4de3260f017e582c6152a27c2e273370a06acca5
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-06-17 15:13:31 +0900

    design: NavBar

--- src/router/Layout.jsx
+++ src/router/Layout.jsx
@@ -1,9 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
+import NavBar from '../components/common/layout/NavBar';
 
 const Layout = () => {
   return (
     <>
       <main>
+        <NavBar />
         <Outlet />
       </main>
     </>
