1ff27d9156b714d57703c1b904a5fc67e6a0c1ef
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-06-19 15:22:09 +0900

    chore: vercel 배포 환경

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@assets": resolve(__dirname, "./src/assets"),
-      "@components": resolve(__dirname, "./src/components"),
-      "@pages": resolve(__dirname, "./src/pages"),
-      "@router": resolve(__dirname, "./src/router"),
-      "@": resolve(__dirname, "./src"),
+      '@assets': resolve(__dirname, './src/assets'),
+      '@components': resolve(__dirname, './src/components'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@router': resolve(__dirname, './src/router'),
+      '@': resolve(__dirname, './src'),
     },
+  },
+  server: {
+    cors: false,
+    proxy: {},
   },
 });
