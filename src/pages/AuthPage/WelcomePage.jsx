34c0aacf91dfccd41cb3818185e2f519141e1cd7
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-06-20 18:26:03 +0900

    design: 초기화면 페이지

---
+++ src/pages/AuthPage/WelcomePage.jsx
@@ -0,0 +1,11 @@
+import WelcomeSection from '../../components/AuthPage/WelcomeSection';
+
+const WelcomePage = () => {
+  return (
+    <div>
+      <WelcomeSection />;
+    </div>
+  );
+};
+
+export default WelcomePage;
