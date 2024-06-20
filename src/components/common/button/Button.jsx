81213138339b75424cd4f23e72b10a90fe947cc3
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-06-20 18:24:23 +0900

    feat: Button 컴포넌트
    - 공통 버튼컴포넌트 제작

---
+++ src/components/common/button/Button.jsx
@@ -0,0 +1,28 @@
+import { forwardRef } from 'react';
+import classNames from 'classnames';
+
+const Button = forwardRef(function Button(
+  { children, className, color = 'beige', ...props },
+  ref,
+) {
+  const buttonColors = {
+    brown: 'bg-[#AC7544] text-white hover:bg-[#8E5B33]',
+    beige: 'bg-[#D8B18E] text-black hover:bg-[#B08A5E]',
+  };
+
+  return (
+    <button
+      ref={ref}
+      className={classNames(
+        'py-2 px-4 rounded-2xl font-semibold border border-transparent',
+        buttonColors[color],
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+});
+
+export default Button;
