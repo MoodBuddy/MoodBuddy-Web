816bc3e248d6ad141ebc7b3d69b04fc3fcbafc7e
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-06-24 17:12:52 +0900

    chore: Resolve merge conflicts with main branch

--- src/constants/MenuList.js
+++ src/constants/MenuList.js
@@ -1,13 +1,7 @@
 export const MenuList = [
-<<<<<<< HEAD
   { id: 1, name: '메인화면', to: '/' },
   { id: 2, name: '일기쓰기', to: '/writing' },
-  { id: 3, name: '검색', to: '/' },
-=======
-  { id: 1, name: '메인화면', to: '/home' },
-  { id: 2, name: '일기쓰기', to: '/' },
   { id: 3, name: '검색', to: '/search' },
->>>>>>> 143692ae77d9263f8b43f0b35e35601af2acf715
   { id: 4, name: '고민상담소', to: '/' },
   { id: 5, name: '마이페이지', to: '/' },
 ];
