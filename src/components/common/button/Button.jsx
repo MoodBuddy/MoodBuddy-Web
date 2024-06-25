4926eb8f695f6f48490c6e8ca6a079a6961b9bdc
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-06-26 01:34:09 +0900

    refactor: Button 컴포넌트

--- src/components/common/button/Button.jsx
+++ src/components/common/button/Button.jsx
@@ -8,6 +8,7 @@ const Button = forwardRef(function Button(
   const buttonColors = {
     brown: 'bg-[#AC7544] text-white hover:bg-[#8E5B33]',
     beige: 'bg-[#D8B18E] text-black hover:bg-[#B08A5E]',
+    lightBeige: 'bg-[#F4EEE9] text-neutral-500 hover:bg-[#ECE6E0]',
     white: 'bg-[#FFFEFD] text-neutral-500 hover:bg-[#DCC6B1]',
   };
 
@@ -15,7 +16,7 @@ const Button = forwardRef(function Button(
     <button
       ref={ref}
       className={classNames(
-        'py-2 px-4 rounded-2xl font-semibold border border-transparent',
+        'py-2 px-4 rounded-2xl font-semibold border',
         buttonColors[color],
         className,
       )}
