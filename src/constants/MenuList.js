5795fa352509e8025192de5556713211655a617f
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-07-05 21:27:42 +0900

    refactor: NavBar, MenuList

--- src/constants/MenuList.js
+++ src/constants/MenuList.js
@@ -1,7 +1,7 @@
 export const MenuList = [
-  { id: 1, name: '메인화면', to: '/home' },
-  { id: 2, name: '일기쓰기', to: '/writing' },
-  { id: 3, name: '검색', to: '/search' },
-  { id: 4, name: '고민상담소', to: '/counseling' },
+  { id: 1, name: 'MAIN', to: '/home' },
+  { id: 2, name: '기록하기', to: '/writing' },
+  { id: 3, name: '추억회상', to: '/search' },
+  { id: 4, name: '쿼디상담소', to: '/counseling' },
   { id: 5, name: '마이페이지', to: '/' },
 ];
