c48a3a3ba7f7fcf6015f3057f696dd807ce5a679
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-07-08 14:27:24 +0900

    refactor : api 파일 수정

--- src/apis/diary.js
+++ src/apis/diary.js
@@ -2,40 +2,35 @@ import client from './client';
 
 const get = async (url) => {
   const res = await client.get(url);
-  return res.data.data;
+  return res?.data;
 };
 
 const post = async (url, data) => {
   const res = await client.post(url, data);
-  return res?.data.data;
+  return res?.data;
 };
 
-export const getFindOne = async (diaryId) => {
+export const getLetter = async () => {
   try {
-    const data = await get(`/api/v1/member/diary/findOne/${diaryId}`);
+    const data = await get(`/api/v1/member/letter`);
     return data;
   } catch (error) {
     throw new Error('데이터 불러오기에 실패하였습니다.');
   }
 };
 
-// Pageable Object 수정해야함
-export const getFindAll = async () => {
+export const getLetterDetails = async (letterId) => {
   try {
-    const data = await get(
-      `/api/v1/member/diary/findAllPageable?page=0&size=20`,
-    );
+    const data = await get(`/api/v1/member/letter/details/${letterId}`);
     return data;
   } catch (error) {
-    console.error('데이터 불러오기에 실패하였습니다.', error);
+    throw new Error('데이터 불러오기에 실패하였습니다.');
   }
 };
 
-export const getFindAllByEmotion = async ({ emotion }) => {
+export const postLetter = async (letterData) => {
   try {
-    const url = `/api/v1/member/diary/findAllByEmotionWithPageable?diaryEmotion=${emotion}&page=0&size=20&sort=string`;
-    const data = await get(url);
-    console.log(data);
+    const data = await post(`/api/v1/member/letter/write`, letterData);
     return data;
   } catch (error) {
     throw new Error('데이터 불러오기에 실패하였습니다.');
