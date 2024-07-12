286b5024d24e306148d952b4119daea8dee2a556
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-07-13 04:11:15 +0900

    feat: 현재까지 작성한 일기 횟수

--- src/apis/user.js
+++ src/apis/user.js
@@ -22,3 +22,12 @@ export const getEmotionStatic = async (date) => {
     throw new Error('데이터 불러오기에 실패하였습니다.');
   }
 };
+
+export const getDiaryNums = async (year) => {
+  try {
+    const data = await get(`/api/v1/member/main/diary-nums?year=${year}`);
+    return data;
+  } catch (error) {
+    throw new Error('데이터 불러오기에 실패하였습니다.');
+  }
+};
