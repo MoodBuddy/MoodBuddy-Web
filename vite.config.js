ad7e72a1a8797011098fcbdbbcd6e5a240556928
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-07-14 23:36:22 +0900

    fix : config proxy 수정

--- vite.config.js
+++ vite.config.js
@@ -16,14 +16,7 @@ export default defineConfig({
     },
   },
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+    proxy: {},
   },
   build: {
     rollupOptions: {
