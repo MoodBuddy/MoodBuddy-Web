160255773df360d65e8210ea92a6c021aaa978e2
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-07-15 23:02:38 +0900

    feat: quddyTi

--- src/components/MyPage/QuddyTi/QuddyTiInfo.jsx
+++ src/components/MyPage/QuddyTi/QuddyTiInfo.jsx
@@ -7,12 +7,29 @@ import FrequencyStats from './FrequencyStats';
 import { useNavigate } from 'react-router-dom';
 import EmotionStats from './EmotionStats';
 import TypeStats from './TypeStats';
+import { getquddyTI } from '../../../apis/user';
+import { useQuery } from '@tanstack/react-query';
 
 const QuddyTiInfo = () => {
   const navigate = useNavigate();
 
   const currentDate = new Date();
   const daysInMonth = getDaysInMonth(currentDate);
+
+  const { isError, data } = useQuery({
+    queryKey: ['quddyTI'],
+    queryFn: getquddyTI,
+    throwOnError: true,
+    onError: (error) => {
+      console.error('Error fetching quddyTI data:', error);
+    },
+  });
+
+  console.log(data);
+
+  if (!data || isError) {
+    return <div className="p-6">데이터가 없습니다.</div>;
+  }
 
   return (
     <div className="p-6">
@@ -24,10 +41,13 @@ const QuddyTiInfo = () => {
           나의 QuddyTI를 알아볼까요?
         </h1>
 
-        <FrequencyStats daysInMonth={daysInMonth} />
-        <TopicStats data={topics} daysInMonth={daysInMonth} />
-        <EmotionStats data={emotions} />
-        <TypeStats />
+        <FrequencyStats
+          daysInMonth={daysInMonth}
+          dailyCount={data.dailyCount}
+        />
+        <TopicStats topicList={topics} data={data} daysInMonth={daysInMonth} />
+        <EmotionStats emotions={emotions} data={data} />
+        <TypeStats data={data} />
       </div>
     </div>
   );
