3801703f0f4d8f538d8594cdf8292c87545b9f96
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-07-18 00:41:59 +0900

    refactor: 북마크페이지 css 수정

--- src/pages/MyPage/BookMarkPage.jsx
+++ src/pages/MyPage/BookMarkPage.jsx
@@ -10,8 +10,8 @@ const BookMarkPage = () => {
         className={`flex flex-col justify-center items-center ${styles.check}`}
       >
         <div className="mt-10">
-          <div className="font-extrabold text-4xl mb-2">북마크 일기</div>
-          <div className="mb-[40px] font-light text-xl">
+          <div className="font-extrabold text-4xl mb-2 ml-2">북마크 일기</div>
+          <div className="mb-[40px] font-light text-xl ml-2">
             내가 북마크한 추억의 일기를 볼 수 있어요.
           </div>
           <DiaryList filterType="bookMark" />
