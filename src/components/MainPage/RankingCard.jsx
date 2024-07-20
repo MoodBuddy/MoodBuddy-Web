7c63d4c54b856018fa93248f0c11b5c2e5c7ca07
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-07-20 20:38:14 +0900

    refactor: 메인페이지 css 수정

--- src/components/MainPage/RankingCard.jsx
+++ src/components/MainPage/RankingCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card';
 import useUserStore from '../../store/userStore';
 import { quddies } from '../../constants/QuddyList';
-import happyQuddy from '@assets/happyQuddy.svg'; // 기본 이미지 import
+import happyQuddy from '@assets/happyQuddy.svg';
 
 const RankingCard = () => {
   const userInfo = useUserStore((state) => ({
@@ -14,12 +14,14 @@ const RankingCard = () => {
   const quddy = quddies.find((quddy) => quddy.emotion === diaryEmotion);
 
   const quddyImgSrc = quddy ? quddy.imgSrc : happyQuddy;
+  const quddyName = quddy ? quddy.nmae : '행복쿼디';
   const quddyAlt = quddy ? quddy.emotion : 'happy';
+  const quddyColor = quddy ? quddy.color : '#D8B18E';
 
   return (
     <Card>
       <div className="justify-center items-center h-full mt-8">
-        <div className="flex gap-8 px-2 items-center ">
+        <div className="flex px-2 items-center">
           <h1 className="text-3xl font-bold">1위 쿼디</h1>
         </div>
         <div className="text-[20px] text-[#7A7A7A] my-3 px-2">
@@ -28,16 +30,16 @@ const RankingCard = () => {
             : '일기를 작성하고 랭킹을 확인해보세요 !'}
         </div>
 
-        <div className="my-12 ml-8">
+        <div className="flex flex-col items-center mt-8 gap-3">
           <img
             src={quddyImgSrc}
             alt={quddyAlt}
             className="w-[193px] h-[204px]"
           />
-        </div>
-
-        <div className="flex justify-end mr-6">
-          <h1 className="text-[40px] font-medium">
+          <p className="font-meetme text-3xl mb-2" style={{ color: quddyColor }}>
+            {quddyName}
+          </p>
+          <h1 className="text-4xl font-medium">
             {maxEmotionNum ? maxEmotionNum : 0} 회
           </h1>
         </div>
