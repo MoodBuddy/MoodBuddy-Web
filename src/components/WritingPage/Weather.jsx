d28126a7d1fb3b8b19fb5828bca62f666426d891
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-07-22 03:18:06 +0900

    fix : 날씨 선택 텍스트 -> 아이콘 변경

--- src/components/WritingPage/Weather.jsx
+++ src/components/WritingPage/Weather.jsx
@@ -1,5 +1,9 @@
 import { useEffect } from 'react';
 import useweatherStore from '../../store/weatherStore';
+import clear from '../../../public/icon/sunIcon.svg';
+import cloudy from '../../../public/icon/cloudIcon.svg';
+import rain from '../../../public/icon/rainIcon.svg';
+import snow from '../../../public/icon/snowIcon.svg';
 
 const Weather = () => {
   const { selectedOption, setSelectedOption } = useweatherStore();
@@ -20,25 +24,25 @@ const Weather = () => {
           onClick={() => handleOptionClick('CLEAR')}
           className={optionClass('CLEAR')}
         >
-          맑음
+          <img src={clear} className="w-7 h-7" />
         </div>
         <div
           onClick={() => handleOptionClick('CLOUDY')}
           className={optionClass('CLOUDY')}
         >
-          구름많음
+          <img src={cloudy} className="w-7 h-7" />
         </div>
         <div
           onClick={() => handleOptionClick('RAIN')}
           className={optionClass('RAIN')}
         >
-          비
+          <img src={rain} className="w-7 h-7" />
         </div>
         <div
           onClick={() => handleOptionClick('SNOW')}
           className={optionClass('SNOW')}
         >
-          눈
+          <img src={snow} className="w-7 h-7" />
         </div>
       </div>
     </>
